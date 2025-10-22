b63f5e3c9fb390c839eb4934dad8c74408360288
daiseek <dnjsgml0578@gmail.com>
2025-10-22 18:01:55 +0900

    feat: seperateCarNames() 정의 - 한 줄로 입력받은 값을 쉽표를 기준으로 구분하는 메서드 정의

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -10,8 +10,9 @@ public class InputView {
         }
     }
     
-    
     // TODO : 한 줄로 입력받은 자동차 이름 목록들을 쉼표를 기준으로 구분하는 메서드 정의
-    
+    public String[] seperateCarNames(String carNames) {
+        return carNames.split(",");
+    }
     // TODO : 시도 횟수를 양의 정수로 받아내는 메서드
 }
