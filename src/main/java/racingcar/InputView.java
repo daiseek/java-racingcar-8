194a85d99b915fa18441b10451f986ad891313f2
daiseek <dnjsgml0578@gmail.com>
2025-10-22 17:56:12 +0900

    chore: TODO 작성 - Validate 클래스 정의 고려하기

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,6 +3,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     // 자동차 이름이 5글자 이하인지 판단하는 메서드
+    // TODO : Validate 클래스 정의를 고려하기
     public void validateCarNames(String carName) {
         if (carName.length() >5 ) {
             throw new IllegalArgumentException("자동차 이름은 5글자 이내여야 합니다.");
