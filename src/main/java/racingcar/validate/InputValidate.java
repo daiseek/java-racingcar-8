813115c9bc5e58e61e757133b809157c7c1e1f58
daiseek <dnjsgml0578@gmail.com>
2025-10-22 18:29:45 +0900

    feat: validateLength() 메서드 정의 - 입력값이 비어있는지 판단 메서드

--- src/main/java/racingcar/validate/InputValidate.java
+++ src/main/java/racingcar/validate/InputValidate.java
@@ -1,4 +1,12 @@
 package racingcar.validate;
 
 public class InputValidate {
+
+    // 입력값이 비어있는지 판단하는 메서드 정의
+    public void validateLength(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력값이 존재해야 합니다.");
+        }
+    }
+
 }
