052d072f59d9674ab11d2798341ce6dea280f603
daiseek <dnjsgml0578@gmail.com>
2025-10-22 18:44:48 +0900

    feat: InputView 클래스에서 TODO 작성 - sperateCarNames() 메서드가 유틸 클래스로 작성되어야 할지 고려

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -16,6 +16,7 @@ public class InputView {
     
     // TODO : 한 줄로 입력받은 자동차 이름 목록들을 쉼표를 기준으로 구분하는 메서드 정의
     // TODO : 입력받은 값에 대한 유효성 검증 필요 - 유효한 값이 무엇인지 경계 정하기
+    // TODO : 해당 메서드는 util 클래스로 분리할 수 있도록 고려
     public String[] separateCarNames(String carNames) {
         return carNames.split(",");
     }
