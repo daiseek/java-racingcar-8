551984d6f7cb3e4272b628a3dde20d5363978fa3
daiseek <dnjsgml0578@gmail.com>
2025-10-22 17:30:21 +0900

    feat: InputView에서 구현할 코드 정리

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,4 +1,9 @@
 package racingcar;
 
 public class InputView {
+    // TODO : 사용자가 입력한 자동차 이름이 5글자 이하인지 판단하는 메서드 정의
+    
+    // TODO : 한 줄로 입력받은 자동차 이름 목록들을 쉼표를 기준으로 구분하는 메서드 정의
+    
+    // TODO : 시도 횟수를 양의 정수로 받아내는 메서드
 }
