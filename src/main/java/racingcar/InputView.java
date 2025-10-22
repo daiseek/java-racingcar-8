141d1c422b33dec0a33b94a98f908a65d8801115
daiseek <dnjsgml0578@gmail.com>
2025-10-22 18:09:52 +0900

    fix: 오탈자 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
     
     // TODO : 한 줄로 입력받은 자동차 이름 목록들을 쉼표를 기준으로 구분하는 메서드 정의
     // TODO : 입력받은 값에 대한 유효성 검증 필요 - 유효한 값이 무엇인지 경계 정하기
-    public String[] seperateCarNames(String carNames) {
+    public String[] separateCarNames(String carNames) {
         return carNames.split(",");
     }
     
