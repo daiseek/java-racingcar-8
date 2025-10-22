3bb76c52a60ef61c5a8577b30c151271fba96295
daiseek <dnjsgml0578@gmail.com>
2025-10-22 18:05:35 +0900

    feat: seperateCarNames() 메서드에서 TODO 추가 - 유효한 값이 무엇인지 경계 설정 필요

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -11,8 +11,10 @@ public class InputView {
     }
     
     // TODO : 한 줄로 입력받은 자동차 이름 목록들을 쉼표를 기준으로 구분하는 메서드 정의
+    // TODO : 입력받은 값에 대한 유효성 검증 필요 - 유효한 값이 무엇인지 경계 정하기
     public String[] seperateCarNames(String carNames) {
         return carNames.split(",");
     }
+    
     // TODO : 시도 횟수를 양의 정수로 받아내는 메서드
 }
