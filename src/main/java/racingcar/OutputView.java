f15fd73674dc37dfae476424cab728e29a2e8d07
daiseek <dnjsgml0578@gmail.com>
2025-10-22 17:30:36 +0900

    feat: OutputView에서 구현할 코드 정리

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,4 +1,8 @@
 package racingcar;
 
 public class OutputView {
+    // TODO : RacingCar를 이용해 할당한 객체가 전진 메서드를 이용해 각 라운드별로 자동차의 전진 횟수를 출력하는 메서드 정의
+    
+    // TODO : 전진 횟수가 제일 많은 레이싱카를 최종 우승자로 출력하는 메서드 정의
+
 }
