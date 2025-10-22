0256f7f41f7f7b86797ff8c18687de2384f95c39
daiseek <dnjsgml0578@gmail.com>
2025-10-22 17:29:43 +0900

    feat: Application이 구현해야할 코드 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,19 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // TODO: 사용자 입력값 받아내기
+
+
+        // TODO: 경주할 자동차를 입력 받기
+
+
+        // TODO: 라운드 횟수 입력 받기
+
+
+        // TODO: 경주할 자동차 객체 할당하기
+
+
+        //
     }
 }
