2808608483644632c8fb1ab431b55c41aa4770cd
daiseek <dnjsgml0578@gmail.com>
2025-10-22 18:41:22 +0900

    feat: Application 클래스에서 반복할 횟수를 입력받도록 메서드 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,18 @@
 package racingcar;
 
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        // TODO: 사용자 입력값 받아내기
-
+        InputView inputView = new InputView();
 
         // TODO: 경주할 자동차를 입력 받기
+
+        // TODO: 사용자 반복할 횟수 입력값 받아내기
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int times = inputView.inputTimes();
 
 
         // TODO: 라운드 횟수 입력 받기
