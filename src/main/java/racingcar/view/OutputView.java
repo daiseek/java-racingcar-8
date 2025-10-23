dc8e6ec666bc56017267a0ca44f3028bc50e382a
daiseek <dnjsgml0578@gmail.com>
2025-10-23 22:58:09 +0900

    feat: printCarPositionPerRound() 메서드 정의

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,6 +5,7 @@ import racingcar.domain.RacingCar;
 public class OutputView {
     // TODO : RacingCar를 이용해 할당한 객체가 전진 메서드를 이용해 각 라운드별로 자동차의 전진 횟수를 출력하는 메서드 정의
     public void printCarPositionPerRound(RacingCar racingCar) {
+        System.out.println(racingCar.getName() + " : " + "-".repeat(racingCar.getPosition()));
     }
     
     // TODO : 전진 횟수가 제일 많은 레이싱카를 최종 우승자로 출력하는 메서드 정의
