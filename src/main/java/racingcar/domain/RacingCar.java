2c89eb9e7b0f663b68938feccd12afe6a42ec2a6
daiseek <dnjsgml0578@gmail.com>
2025-10-23 22:17:32 +0900

    feat: RacingCar 정적 팩토리 메서드 정의

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -2,6 +2,10 @@ package racingcar.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RacingCar {
+
+    // 기본 생성자는 사용하지 못하게 private 접근자 선언
+    private RacingCar(String name) { }
+    
     // TODO: 객체마다 0과 9 사이 랜덤한 값을 메서드 내 지역 변수로 가짐
 
     // TODO: 랜덤값이 4 이상일 때 전진하는 메서드 필요? - 전진 횟수를 정수, 하이픈을 상수로 관리하기, 해당 메서드를 RacingCar가 정의해야 할까?
@@ -12,10 +16,10 @@ public class RacingCar {
         }
     return 0;}
 
-        
-
-
     // TODO: 명확한 객체 생성자 사용하기
+    public static RacingCar from(String carName) {
+        return new RacingCar(carName);
+    }
 
 
 }
