a9de60c093cd7ef6ab98302f50b5adacabc0efa1
daiseek <dnjsgml0578@gmail.com>
2025-10-25 02:46:44 +0900

    feat: 기능 분리로 인해 기존 로직 제거 및 findWinnderRacingCars() 메서드 호출

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import java.util.List;
 import java.util.stream.Collectors;
 import racingcar.domain.RacingCar;
+import racingcar.util.OutputUtil;
 
 public class OutputView {
     // TODO : RacingCar를 이용해 할당한 객체가 전진 메서드를 이용해 각 라운드별로 자동차의 전진 횟수를 출력하는 메서드 정의
@@ -14,17 +15,7 @@ public class OutputView {
     // TODO : 동점자가 있을 경우 쉼표로 구분하여 출력
     // TODO : car들을 여러개 받아서 어떻게 최다 position을 가진 car 객체를 찾을 수 있는지 고민
     public void printWinnerCarName(List<RacingCar> racingCar) {
-        // 1. 모든 자동차 객체의 position 중에서 최대값 찾기
-        int maxPosition = racingCar.stream()
-                .mapToInt(RacingCar::getPosition) // racingCar 객체들의 position을 순차적으로 int 타입으로 가져옴
-                .max() // 불러온 position 값에서 가장 큰 값 찾기
-                .orElse(0); // max() 결과가 null 일때 0을 반환
-
-        // 2. 우승자 필터링하기
-        List<RacingCar> winners = racingCar.stream()
-                // position 값이 maxPosition과 일치하는 racingCar 객체만 남도록 필터링
-                .filter(car -> car.getPosition() == maxPosition)
-                .toList(); // List 형태로 반환
+        List<RacingCar> winners = OutputUtil.findWinnerRacingCars(racingCar);
 
         String winnerNames = winners.stream()
                 .map(RacingCar::getName)
