271deb0e3f6ac68964d0f397d94e5e66a2ee9633
daiseek <dnjsgml0578@gmail.com>
2025-10-25 03:08:22 +0900

    feat: makeRacingCars() 메서드 정의

--- src/main/java/racingcar/util/InputUtil.java
+++ src/main/java/racingcar/util/InputUtil.java
@@ -1,4 +1,17 @@
 package racingcar.util;
 
+import java.util.Arrays;
+import java.util.List;
+import racingcar.domain.RacingCar;
+
 public class InputUtil {
+
+    // 입력받은 이름을 stream을 이용해 순차적으로 RacingCar 객체로 만드는 메서드
+    public static List<RacingCar> makeRacingCars(String[] racingCarNames) {
+        return Arrays.stream(racingCarNames)
+                .map(String::trim)
+                .filter(name -> !name.isEmpty())
+                .map(RacingCar::of)
+                .toList();
+    }
 }
