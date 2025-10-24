9b3945b74cf9a650077467fb6e4f80cb9063ba52
daiseek <dnjsgml0578@gmail.com>
2025-10-25 03:56:22 +0900

    feat : OutputView 단위테스트 파일 생성 및 테스트 메서드 분리

---
+++ src/test/java/racingcar/util/OutputUtilTest.java
@@ -0,0 +1,34 @@
+package racingcar.util;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OutputUtilTest {
+
+    @Test
+    void 우승자_찾기_단일_우승자() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 우승자_찾기_공동_우승자() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 우승자_이름_연결() {
+        // given
+
+        // when
+
+        // then
+    }
+}
---
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -0,0 +1,25 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OutputViewTest {
+
+    @Test
+    void 라운드별_자동차_위치_출력() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 우승자_출력() {
+        // given
+
+        // when
+
+        // then
+    }
+}
