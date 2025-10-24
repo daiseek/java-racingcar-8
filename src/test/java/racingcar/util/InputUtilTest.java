3ad89b1bccedb420d08aa659542595f9447b385b
daiseek <dnjsgml0578@gmail.com>
2025-10-25 03:55:55 +0900

    feat : InputValidate 단위테스트 파일 생성 및 테스트 메서드 분리

---
+++ src/test/java/racingcar/util/InputUtilTest.java
@@ -0,0 +1,34 @@
+package racingcar.util;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InputUtilTest {
+
+    @Test
+    void 자동차_객체_생성() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 빈_이름_필터링() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 공백_제거() {
+        // given
+
+        // when
+
+        // then
+    }
+}
---
+++ src/test/java/racingcar/validate/InputValidateTest.java
@@ -0,0 +1,80 @@
+package racingcar.validate;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputValidateTest {
+
+    @Test
+    void 입력값_길이_검증_성공() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 입력값_길이_검증_실패() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 이름_길이_검증_성공() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 이름_길이_검증_실패() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 양수_검증_성공() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 양수_검증_실패() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 이름_배열_검증_성공() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 이름_배열_검증_실패() {
+        // given
+
+        // when
+
+        // then
+    }
+}
