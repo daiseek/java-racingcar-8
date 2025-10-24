b9eb7ee674cb4bb2f5330d0441d3f5d380242aa5
daiseek <dnjsgml0578@gmail.com>
2025-10-25 03:55:27 +0900

    feat : InputView 단위테스트 파일 생성 및 테스트 메서드 분리

---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,43 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InputViewTest {
+
+    @Test
+    void 자동차_이름_입력_받기() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 시도_횟수_입력_받기() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 이름_분리() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 개별_이름_검증() {
+        // given
+
+        // when
+
+        // then
+    }
+}
