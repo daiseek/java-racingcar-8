0c707f449fd2282a63090ebb31392be96df11ca4
daiseek <dnjsgml0578@gmail.com>
2025-10-25 02:54:09 +0900

    fix: OutputView 클래스 기능 구현 확인 및 TODO 삭제

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,14 +5,11 @@ import racingcar.domain.RacingCar;
 import racingcar.util.OutputUtil;
 
 public class OutputView {
-    // TODO : RacingCar를 이용해 할당한 객체가 전진 메서드를 이용해 각 라운드별로 자동차의 전진 횟수를 출력하는 메서드 정의
+
     public void printCarPositionPerRound(RacingCar racingCar) {
         System.out.println(racingCar.getName() + " : " + "-".repeat(racingCar.getPosition()));
     }
     
-    // TODO : 최종 라운드에서 전진 횟수가 제일 많은 레이싱카를 최종 우승자로 출력하는 메서드 정의
-    // TODO : 동점자가 있을 경우 쉼표로 구분하여 출력
-    // TODO : car들을 여러개 받아서 어떻게 최다 position을 가진 car 객체를 찾을 수 있는지 고민
     public void printWinnerCarName(List<RacingCar> racingCar) {
         List<RacingCar> winners = OutputUtil.findWinnerRacingCars(racingCar);
         String winnerNames = OutputUtil.joinWinnerNames(winners);
