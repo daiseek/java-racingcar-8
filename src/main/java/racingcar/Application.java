ab72496179ca3dbefe785023d58aff1833b5f9af
daiseek <dnjsgml0578@gmail.com>
2025-10-25 03:50:29 +0900

    feat : 자동차 경주 미션 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,7 @@ package racingcar;
 import java.util.List;
 import racingcar.domain.RacingCar;
 import racingcar.util.InputUtil;
+import racingcar.validate.InputValidate;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -12,27 +13,24 @@ public class Application {
 
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
+        InputValidate inputValidate = new InputValidate();
 
-        // TODO: 여러 입력값을 넣어보며 유효성 검증해보기
-        // TODO: 경주할 자동차를 입력 받기
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         String input = inputView.inputCarNames();
-        String validatedInput = inputView.validateCarName(input);
-        String[] racingCarNames = inputView.separateCarNames(validatedInput);
+        String[] racingCarNames = inputView.separateCarNames(input);
 
-        // TODO: 경주할 자동차 객체 할당하기
+        for (String name : racingCarNames) {
+            inputValidate.validateCarName(name.trim());
+        }
+
         // 이름으로 RacingCar 객체 생성
         List<RacingCar> racingCars = InputUtil.makeRacingCars(racingCarNames);
 
-
-        // TODO: 사용자 반복할 횟수 입력값 받아내기
         System.out.println("시도할 횟수는 몇 회인가요?");
 
-        // TODO: 라운드 횟수 입력 받기
         int racingTimes = inputView.inputTimes();
 
-        // TODO : 게임 진행하기
         for (int round = 0 ; round < racingTimes; round++) {
 
             for (RacingCar racingCar : racingCars) {
