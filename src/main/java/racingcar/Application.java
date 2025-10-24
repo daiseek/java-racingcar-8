0aae4ade469cc52f7b689d05f1dd4ba92c94d435
daiseek <dnjsgml0578@gmail.com>
2025-10-25 03:10:29 +0900

    fix: 사용되지 않는 클래스 호출부 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,9 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import racingcar.domain.RacingCar;
 import racingcar.util.InputUtil;
 import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
