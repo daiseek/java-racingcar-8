2f9e9c5b9540224aa57c224c958e2f668d60c6db
daiseek <dnjsgml0578@gmail.com>
2025-10-25 15:50:49 +0900

    test : OutputView 단위 테스트 작성

--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -1,25 +1,21 @@
 package racingcar.view;
 
 import org.junit.jupiter.api.Test;
+import racingcar.domain.RacingCar;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+// System.out.println() 메서드를 사용해 출력하는 메서드가 주된 기능므로 통합 테스트에서 검증
 class OutputViewTest {
 
     @Test
     void 라운드별_자동차_위치_출력() {
-        // given
-
-        // when
-
-        // then
+        assertThat(true).isTrue(); // 더미 테스트
     }
 
     @Test
     void 우승자_출력() {
-        // given
-
-        // when
-
-        // then
+        assertThat(true).isTrue(); // 더미 테스트
     }
 }
