e0785680f37b87ee72f7eb3025e46d9e6c069e72
daiseek <dnjsgml0578@gmail.com>
2025-10-25 15:51:36 +0900

    test : InputView 각 메서드 별로 단위 테스트 작성

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -2,42 +2,47 @@ package racingcar.view;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputViewTest {
 
     @Test
-    void 자동차_이름_입력_받기() {
-        // given
-
-        // when
-
-        // then
-    }
-
-    @Test
-    void 시도_횟수_입력_받기() {
-        // given
-
-        // when
-
-        // then
-    }
-
-    @Test
     void 이름_분리() {
         // given
+        InputView inputView = new InputView();
+        String input = "pobi,woni,jun";
 
         // when
+        String[] result = inputView.separateCarNames(input);
 
         // then
+        assertThat(result).containsExactly("pobi", "woni", "jun");
     }
 
     @Test
-    void 개별_이름_검증() {
+    void 이름_분리_공백_포함() {
         // given
+        InputView inputView = new InputView();
+        String input = "pobi, woni , jun";
 
         // when
+        String[] result = inputView.separateCarNames(input);
 
         // then
+        assertThat(result).containsExactly("pobi", "woni ", " jun");
     }
+
+    @Test
+    void 이름_분리_빈_이름_포함() {
+        // given
+        InputView inputView = new InputView();
+        String input = "pobi,,jun";
+
+        // when
+        String[] result = inputView.separateCarNames(input);
+
+        // then
+        assertThat(result).containsExactly("pobi", "", "jun");
+    }
+
 }
