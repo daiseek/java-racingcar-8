b4a94113ad326eaf9475c6162ed865973cd7eff5
daiseek <dnjsgml0578@gmail.com>
2025-10-25 15:59:35 +0900

    fix : 사용되지 않는 클래스 호출부 제거

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -2,7 +2,7 @@ package racingcar.view;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 
 class InputViewTest {
 
--- src/test/java/racingcar/view/OutputViewTest.java
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -1,8 +1,6 @@
 package racingcar.view;
 
 import org.junit.jupiter.api.Test;
-import racingcar.domain.RacingCar;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
